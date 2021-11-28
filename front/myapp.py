9e6a8253dc93f111a04f5ff4b2ade425e44b96a7
naem1023 <relilau00@gmail.com>
2021-11-29 03:39:12 +0900

    Add async chat in front

--- front/myapp.py
+++ front/myapp.py
@@ -2,29 +2,25 @@
 from flask import Flask, render_template, request
 from transformers import AutoModel, AutoConfig, AutoTokenizer, GPT2LMHeadModel, GPT2Tokenizer
 import requests
+
+import aiohttp
+import asyncio
+
+
 app = Flask(__name__)
 @app.route("/")
 def home(): 
 	return render_template("home.html")
 
 @app.route("/get")
-def get_bot_response():
+async def get_bot_response():
     userText = request.args.get('msg')
+    print(userText)
     data = {'data':userText}
-    res = requests.post('127.0.0.1:8080', data=data)
-    res_json = res.json()
-
-    # input_ids= tokenizer.encode(userText, return_tensors= 'pt')
-    # predict= model.generate(
-    #     input_ids, 
-    #     max_length=50, 
-    #     num_beams=5, 
-    #     no_repeat_ngram_size=2, 
-    #     early_stopping=True)
-
-    # result= tokenizer.decode(predict[0], skip_special_tokens= True)
-
-    return res_json['result']
+    async with aiohttp.ClientSession() as session:
+        url = 'http://172.28.188.230:8080/chat'
+        async with session.post(url, json=data) as resp:
+            return ((await resp.json())['result'])
 
 if __name__ == "__main__":
     # model_name= 'gpt2'
