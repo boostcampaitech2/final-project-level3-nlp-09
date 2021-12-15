65eadb49087fbd2051506b59c1242833cb972e47
Jay-Ppark <godjaehyung@gmail.com>
2021-12-15 19:36:02 +0000

    add: buttons and solve error when sending messages

--- boot/test1/test.js
+++ boot/test1/test.js
@@ -1,5 +1,29 @@
-$(document).ready(function(){
-    $('#action_menu_btn').click(function(){
-        $('.action_menu').toggle();
-    });
-        });
+function getBotResponse() {
+    var rawText = $("#textInput").val();
+    console.log(rawText);
+    $("#textInput").val("");
+    var userHtml = '<p class="userText"><span>' + rawText + "</span></p>";
+    $("#userchatbox").append(userHtml);
+  }
+function getHintResponse(trial) {
+    if(trial>5){
+      document.getElementById("hintButton").disabled = false;
+    }
+    else{
+      document.getElementById("hintButton").disabled = true;
+    }
+  }
+  var trial=0;
+  $("#textInput").keypress(function(e) {
+    if (e.keyCode == '13') {
+      getBotResponse();
+      trial+=1;
+      getHintResponse(trial)
+    }
+  });
+  $("#hintButton").click(function() {
+    getHintResponse(trial)
+    console.log("Hint");
+    console.log(trial);
+    trial += 1;
+  });
