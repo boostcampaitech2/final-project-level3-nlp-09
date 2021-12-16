2450679025bf595d0c07cfd71339fbfe79f01564
naem1023 <relilau00@gmail.com>
2021-12-17 05:39:31 +0900

    Fix chat flow for QA

--- boot/test1/test.js
+++ boot/test1/test.js
@@ -4,21 +4,16 @@ function getBotResponse() {
     var rawText = $("#textInput").val();
     console.log(rawText);
     $("#textInput").val("");
-    if(rawText >= 1 && rawText <=6){
-      var userHtml = '<div class="media media-chat media-chat-reverse"><img class="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..."><div class="media-body"><p class="userText"><span>' + rawText + "</span></p></div></div>";
-      $("#chat-content").append(userHtml);
-      var botHtml = '<div class="media media-chat"><img class="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..."><div class="media-body"><p class="userText"><span>' + rawText + "번 테마를 골랐습니다." + "</span></p></div></div>";
-      $("#chat-content").append(botHtml);
-    }
-    else{
-      var userHtml = '<div class="media media-chat media-chat-reverse"><img class="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..."><div class="media-body"><p class="userText"><span>' + rawText + "</span></p></div></div>";
-      $("#chat-content").append(userHtml);
-      trial+=1;
-      calculateTrial();
-      console.log(trial);
-    }
+    var userHtml = '<div class="media media-chat media-chat-reverse"><img class="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..."><div class="media-body"><p class="userText"><span>' + rawText + "</span></p></div></div>";
+    $("#chat-content").append(userHtml);
+    trial += 1;
+    calculateTrial();
+    console.log(trial);
   }
 function getHintResponse(trial) {
+  /*
+  엔터가 눌릴 때마다, hint 버튼을 활성화시킬지 결정
+  */
     if(trial>=5){
       document.getElementById("hintButton").disabled = false;
     }
@@ -34,19 +29,39 @@ function calculateCorrect() {
   var t_element = document.getElementById("correctCount");
   t_element.innerText="맞힌 갯수 :" + correctNum;
 }
-  $("#textInput").keypress(function(e) {
-    if (e.keyCode == '13') {
-      getBotResponse();
-      getHintResponse(trial)
-      calculateTrial();
-      calculateCorrect();
+
+count = 0
+
+$("#textInput").keypress(function(e) {
+  if (e.keyCode == '13') {
+    count += 1
+    if (count >  10){
+      if ($("#textInput").val() == "재시작") {
+        count = 0;
+        window.location.reload();
+      }
+      return;
     }
-  });
-  $("#hintButton").click(function() {
+    // .scrollIntoView({block: "start", behavior: "smooth"});
+    $("#chat-content").scrollTop($("#chat-content").height());
+    console.log($("#chat-content").height())
+    //   $("html, chat-content").animate({
+      //     scrollTop: $(// 
+        //       'html, chat-content').get(0).scrollHeight 
+        // }
+        // }, 2000);
+    getBotResponse();
     getHintResponse(trial)
-    console.log("Hint");
-    console.log(trial);
-    trial=0;
     calculateTrial();
-    getHintResponse(trial)
-  });
+    calculateCorrect();
+    document.getElementById("userInput")
+  }
+});
+$("#hintButton").click(function() {
+  getHintResponse(trial)
+  console.log("Hint");
+  console.log(trial);
+  trial=0;
+  calculateTrial();
+  getHintResponse(trial)
+});
