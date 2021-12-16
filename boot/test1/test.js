4ea27a8b5322aee0a81dacfb02f44343a1f1ff93
Jay-Ppark <godjaehyung@gmail.com>
2021-12-16 01:52:21 +0000

    add: count function

--- boot/test1/test.js
+++ boot/test1/test.js
@@ -1,9 +1,22 @@
+var trial = 0;
 function getBotResponse() {
     var rawText = $("#textInput").val();
     console.log(rawText);
     $("#textInput").val("");
-    var userHtml = '<p class="userText"><span>' + rawText + "</span></p>";
-    $("#userchatbox").append(userHtml);
+    if(rawText >= 1 && rawText <=6){
+      var userHtml = '<div class="media media-chat media-chat-reverse"><img class="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..."><div class="media-body"><p class="userText"><span>' + rawText + "</span></p></div></div>";
+      $("#chat-content").append(userHtml);
+      var botHtml = '<div class="media media-chat"><img class="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..."><div class="media-body"><p class="userText"><span>' + rawText + "번 테마를 골랐습니다." + "</span></p></div></div>";
+      $("#chat-content").append(botHtml);
+    }
+    else{
+      var userHtml = '<div class="media media-chat media-chat-reverse"><img class="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="..."><div class="media-body"><p class="userText"><span>' + rawText + "</span></p></div></div>";
+      $("#chat-content").append(userHtml);
+      trial+=1;
+      var t_element = document.getElementById("trialCount");
+      t_element.innerText=trial;
+      console.log(trial);
+    }
   }
 function getHintResponse(trial) {
     if(trial>5){
@@ -13,11 +26,9 @@ function getHintResponse(trial) {
       document.getElementById("hintButton").disabled = true;
     }
   }
-  var trial=0;
   $("#textInput").keypress(function(e) {
     if (e.keyCode == '13') {
       getBotResponse();
-      trial+=1;
       getHintResponse(trial)
     }
   });
