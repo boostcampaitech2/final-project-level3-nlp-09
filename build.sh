38bb1b0d42182409c367d0988e24f204947b5715
naem1023 <relilau00@gmail.com>
2021-12-20 04:23:40 +0900

    Add build.sh for building API server

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-cp -r QA_model api/
-docker-compose -d up --build api app rabbit-mq
+# cp -r QA_model api/
+# docker-compose -d up --build api app rabbit-mq
+docker stop final-project-api
+docker build --tag final-project-api ./api/
+docker run --gpus all -d -p 9090:9090 --name final-project-api final-project-level3-nlp-09_api:latest
