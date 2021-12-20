f10f18183b4df5d49a8003efa2fc62c363bcd236
naem1023 <relilau00@gmail.com>
2021-12-20 21:40:40 +0900

    Remove unuesd dir

--- api/QA_model/upload_model.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer
-
-model = AutoModelForQuestionAnswering.from_pretrained('./models/klue/roberta-large5/checkpoint-28500/')
-model.push_to_hub("ko-mrc-model")
-
-tokenizer = AutoTokenizer.from_pretrained('./models/klue/roberta-large5/checkpoint-28500/')
-tokenizer.push_to_hub("ko-mrc-model")
