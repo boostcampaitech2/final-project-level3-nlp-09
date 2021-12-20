b9abbc302504123de89e547a80780c4b8d3b4bb7
naem1023 <relilau00@gmail.com>
2021-12-21 02:34:19 +0900

    Fix build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # docker-compose -d up --build api app rabbit-mq
 rm -rf api/QA_model
 cp -r QA_model api/
-docker stop final-project-api
-docker rm final-project-api
+docker stop final-api
+docker rm final-api
 docker build --tag final-project-api ./api/
-docker run --gpus all -d -p 9090:9090 --name final-project-api final-project-level3-nlp-09_api:latest
+docker run --gpus all -d -p 9090:9090 --name final-api final-project-api:latest
