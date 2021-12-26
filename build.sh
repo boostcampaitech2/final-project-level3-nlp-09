4383abd89266240adee0cfc8cf3c633b148947a3
naem1023 <relilau00@gmail.com>
2021-12-27 04:01:43 +0900

    Fix docker context error

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ cp -r QA_model/data api/bool-qa/data
 # Build Extracion-based MRC model container
 docker stop em-api-server
 docker rm em-api-server
-docker build --tag em-api ./api/extraction-mrc
+docker build --tag em-api -f ./api/extraction-mrc/Dockerfile .
 docker run --gpus all -d -p 9090:9090 --name em-api-server em-api:latest
 
 # Build Boolean QA model container
 docker stop bm-api-server
 docker rm bm-api-server
-docker build --tag bm-api ./api/bool-qa
+docker build --tag bm-api -f ./api/bool-qa/Dockerfile ./api
 docker run --gpus all -d -p 9091:9091 --name bm-api-server bm-api:latest
